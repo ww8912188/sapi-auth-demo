9e1674156cb7f7be31a3696a27cb469f3d216de9
Sarcy <yinnan891@gmail.com>
2021-01-28 16:33:07 +0800

    debug

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const crypto = require("crypto")
 const queryString = require('query-string')
 const axios = require('axios')
 
-const API_KEY = ''
-const API_SECRET = ''
-const HOST = 'https://api.binance.com'
+const API_KEY = 'QmC53GMFUP2YTamtCKZn3MHuQoK6YRl6v13QkfE6w5ALRZzhNdgSn1omiVxBm2w1'
+const API_SECRET = '10b44wmFANPUHE7qaTacLKR43vuZcSFKvLMrn1ajODCA6nlcjDSecMFbXSDj1EiX'
+// const HOST = 'https://api.binance.com'
+const HOST = 'http://internal-tk-dev-sapi-alb-1564553427.ap-northeast-1.elb.amazonaws.com:9056'
 
 const genSig = (query, secret) => {
   let signature = crypto.createHmac("sha256", secret).update(query).digest("hex")
